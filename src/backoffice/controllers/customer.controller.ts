6a43730a278c1ca817eca11be53462cb7d723470
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-06-07 19:42:52 -0300

    add controllers

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,4 +1,20 @@
 import { Controller } from '@nestjs/common';
 
 @Controller()
-export class ControllerController {}
+export class CustomerController {
+  get() {
+    return 'Obter os clientes';
+  }
+
+  post() {
+    return 'Criar cliente';
+  }
+
+  put() {
+    return 'Atualizar um cliente';
+  }
+
+  delete() {
+    return 'Remover um cliente';
+  }
+}
