acbdb01a8933a8fd9782d745db0ca6e4ddc72f32
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-06-08 19:24:21 -0300

    add contracts

---
+++ src/backoffice/contracts/customer.contracts.ts
@@ -0,0 +1,20 @@
+import { Flunt } from 'src/utils/flunt';
+import { Customer } from '../models/customer.model';
+import { Contract } from './contract';
+
+export class CreateCustomerContract implements Contract {
+  errors: any[];
+  validate(model: Customer): boolean {
+    const flunt = new Flunt();
+
+    flunt.hasMinLen(model.name, 5, 'Nome inválido');
+    flunt.isEmail(model.email, 'E-mail inválido');
+    flunt.isFixedLen(model.document, 11, 'CPF inválido');
+    flunt.hasMinLen(model.name, 5, 'Senha inválida');
+    flunt.hasMinLen(model.name, 5, 'Nome inválido');
+
+    this.errors = flunt.errors;
+
+    return flunt.isValid();
+  }
+}
