5344c98a973107da4a7000456e500d43af523c04
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-06-08 18:47:58 -0300

    add models

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -7,34 +7,33 @@ import {
   Param,
   Body,
 } from '@nestjs/common';
+import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.model';
 
 @Controller('v1/customers')
 export class CustomerController {
   @Get()
   get() {
-    return 'Obter os clientes';
+    return new Result(null, true, [], null)
   }
 
   @Get(':document')
-  getById(@Param('document') document) {
-    return 'Obter o cliente ' + document;
+  getById(@Param('document') document: string) {
+    return new Result(null, true, {}, null)
   }
 
   @Post()
-  post(@Body() body) {
-    return body;
+  post(@Body() body: Customer) {
+    return new Result('Cliente alterado com sucesso!', true, body, null)
   }
 
   @Put(':document')
   put(@Param('document') document, @Body() body) {
-    return {
-      customer: document,
-      data: body,
-    };
+    return new Result('Cliente alterado com sucesso!', true, body, null)
   }
 
   @Delete(':document')
   delete(@Param('document') document) {
-    return 'Remover um cliente';
+    return new Result('Cliente removido com sucesso', true, null, null);
   }
 }
