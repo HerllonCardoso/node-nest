7e08a44b2bd678a2665222c1db82d181e6ebdd7c
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-06-09 19:49:19 -0300

    feat: add interceptors

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -6,7 +6,10 @@ import {
   Delete,
   Param,
   Body,
+  UseInterceptors
 } from '@nestjs/common';
+import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
+import { CreateCustomerContract } from '../contracts/customer.contracts';
 import { Customer } from '../models/customer.model';
 import { Result } from '../models/result.model';
 
@@ -23,6 +26,7 @@ export class CustomerController {
   }
 
   @Post()
+  @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
   post(@Body() body: Customer) {
     return new Result('Cliente alterado com sucesso!', true, body, null)
   }
