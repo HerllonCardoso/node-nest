d2001c40b18b4d5ceb211da5f8f61a66f0e6b837
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-06-14 21:17:45 -0300

    create customer

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -6,34 +6,56 @@ import {
   Delete,
   Param,
   Body,
-  UseInterceptors
+  UseInterceptors,
 } from '@nestjs/common';
 import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
 import { CreateCustomerContract } from '../contracts/customer.contracts';
 import { CreateCustomerDTO } from '../dtos/create-customer-dto';
 import { Result } from '../models/result.model';
+import { User } from '../models/user.model';
+import { Customer } from '../models/customer.model';
+import { AccountService } from '../services/account.service';
+import { CustomerService } from '../services/customer.service';
 
 @Controller('v1/customers')
 export class CustomerController {
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly customerService: CustomerService,
+  ) {}
   @Get()
   get() {
-    return new Result(null, true, [], null)
+    return new Result(null, true, [], null);
   }
 
   @Get(':document')
   getById(@Param('document') document: string) {
-    return new Result(null, true, {}, null)
+    return new Result(null, true, {}, null);
   }
 
   @Post()
   @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
-  post(@Body() body: CreateCustomerDTO) {
-    return new Result('Cliente alterado com sucesso!', true, body, null)
+  async post(@Body() model: CreateCustomerDTO) {
+    const user = await this.accountService.create(
+      new User(model.document, model.password, true),
+    );
+    const customer = new Customer(
+      model.name,
+      model.document,
+      model.email,
+      null,
+      null,
+      null,
+      null,
+      user,
+    );
+    const res = await this.customerService.create(customer);
+    return new Result('Cliente criado com sucesso!', true, res, null);
   }
 
   @Put(':document')
   put(@Param('document') document, @Body() body) {
-    return new Result('Cliente alterado com sucesso!', true, body, null)
+    return new Result('Cliente alterado com sucesso!', true, body, null);
   }
 
   @Delete(':document')
